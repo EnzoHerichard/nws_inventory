751b2c9ee77f9edcc5bd99e8eb44aafee2e88366
EnzoHerichard <herichardenzo@gmail.com>
2022-11-16 16:51:26 +0100

    initsql

--- init.sql
+++ init.sql
@@ -5,6 +5,8 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+CREATE DATABASE IF NOT EXISTS nwsmaterials;
+
 USE `nwsmaterials`;
 
 SET NAMES utf8mb4;
